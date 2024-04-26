17e51ec7ca6093697cc9946b12c9abd7fa12b165
Daniel R. Gomes <dr987100@gmail.com>
2024-04-26 14:00:54 -0300

    feat: Adicionando exercicio do modulo UML e Diagramacao

---
+++ POO/UML/uml_class/src/Iphone.java
@@ -0,0 +1,17 @@
+public class Iphone implements Navegador, Reprodutor, Telefone {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public void exibirPagina() {}
+    public void adicionarNovaAba() {}
+    public void atualizarPagina(){}
+
+    public void ligar() {}
+    public void atender() {}
+    public void iniciarCorrerioVoz() {}
+
+    public void tocar(){} 
+    public void pausar(){}
+    public void selecionarMusica(){}
+}
